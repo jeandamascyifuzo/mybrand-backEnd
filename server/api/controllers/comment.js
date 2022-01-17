69a3afcada68ebf112dcb1f9f8d4d36ca648019b
cyifuzo <jeancyifuzodamas@gmail.com>
2022-01-16 23:38:55 -0500

    Comments

---
+++ server/api/controllers/comment.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const Comments = require('../models/comment')
+
+exports.addComment = async(req,res)=>{
+    try {
+        const data = (req.body)
+        const comment = new Comments({...data, blog:req.params.id})
+
+        await comment.save()
+        res.status(200).send(comment)
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+}
+
+exports.getComments = async(req,res)=>{
+    
+        const comments = await Comments.findOne({blog:req.params.id})
+        res.status(200).send(comments)
+}
